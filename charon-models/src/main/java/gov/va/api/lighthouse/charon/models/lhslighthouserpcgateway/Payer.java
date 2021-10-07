9bfe417a70ac6d875c8c97e745e1098089af1d49
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-10-07 14:00:50 -0400

    add payer fields (#120)

--- charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/Payer.java
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/Payer.java
@@ -2,4 +2,16 @@ package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
 
 public class Payer {
   public static final String FILE_NUMBER = "365.12";
+
+  public static final String PAYER_NAME = "#.01";
+
+  public static final String VA_NATIONAL_ID = ".02";
+
+  public static final String CMS_NATIONAL_ID = ".03";
+
+  public static final String EDI_ID_NUMBER_PROF = ".05";
+
+  public static final String EDI_ID_NUMBER_INST = ".06";
+
+  public static final String DEACTIVATED = ".07";
 }

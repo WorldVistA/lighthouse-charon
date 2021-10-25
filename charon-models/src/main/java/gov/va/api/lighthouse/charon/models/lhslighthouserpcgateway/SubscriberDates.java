f151f8ca65aed71dd373658c6bbe82394e74ace1
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-10-25 14:47:42 -0400

    API-000-subscriber-dates (#126)

---
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/SubscriberDates.java
@@ -0,0 +1,13 @@
+package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
+
+public class SubscriberDates {
+  public static final String FILE_NUMBER = "365.02";
+
+  public static final String SEQUENCE = "#.01";
+
+  public static final String DATE = "#.02";
+
+  public static final String DATE_QUALIFIER = "#.03";
+
+  public static final String DATE_FORMAT = "#.04";
+}

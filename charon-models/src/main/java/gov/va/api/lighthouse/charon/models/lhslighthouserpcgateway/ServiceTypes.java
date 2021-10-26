a099b9f809717c7456106bd7118c7b90bf1a29e9
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-10-26 15:29:21 -0400

    ServiceTypes file definition (#128)

---
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/ServiceTypes.java
@@ -0,0 +1,7 @@
+package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
+
+public class ServiceTypes {
+    public static final String FILE_NUMBER = "365.292";
+
+    public static final String SERVICE_TYPES = "#.01";
+}

c06fa34a750dac1e4dbb2d52de563b2886b0556f
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-10-27 14:31:40 -0400

    EligibilityBenefit: Filenumber 2.322 to 365.02

--- charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/EligibilityBenefit.java
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/EligibilityBenefit.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
     date = "2021-07-02T14:23:27Z",
     comments = "Generated using insuranceElementList.xlsx version 183.0.")
 public class EligibilityBenefit {
-  public static final String FILE_NUMBER = "2.322";
+  public static final String FILE_NUMBER = "365.02";
   public static final String ELIGIBILITY_BENEFIT_INFO = "#.02";
   public static final String COVERAGE_LEVEL = "#.03";
   public static final String INSURANCE_TYPE = "#.05";

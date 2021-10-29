feb8e63f792096410019de288ea2ab642fb398d7
jabando-va <42628485+jabando-va@users.noreply.github.com>
2021-10-29 13:13:00 -0400

    fixed file number (#132)

--- charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/N277EdiIdNumber.java
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/N277EdiIdNumber.java
@@ -1,7 +1,7 @@
 package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
 
 public class N277EdiIdNumber {
-  public static final String FILE_NUMBER = "#36.017";
+  public static final String FILE_NUMBER = "36.017";
   public static final String N277EDI_ID_NUMBER = "#.01";
   public static final String N277DATE_EDI_ID_NUMBER = "#.02";
   public static final String N277EDI_TYPE = "#.03";

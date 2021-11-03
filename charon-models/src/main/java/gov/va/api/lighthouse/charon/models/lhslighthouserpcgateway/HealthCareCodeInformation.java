052702f93127c63f534bf032aff2493c09ce4797
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-11-03 09:37:56 -0400

    HealthCareCodeInformation constants (#133)

---
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/HealthCareCodeInformation.java
@@ -0,0 +1,9 @@
+package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
+
+public class HealthCareCodeInformation {
+  public static final String FILE_NUMBER = "365.01";
+  public static final String SEQUENCE = "#.01";
+  public static final String DIAGNOSIS_CODE = "#.02";
+  public static final String DIAGNOSIS_CODE_QUALIFIER = "#.03";
+  public static final String PRIMARY_OR_SECONDARY = "#.04";
+}

1bcdc190013316a55e574f49185ac29e7c615251
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-11-04 15:15:45 -0400

    SubscriberAdditionalInfo constants (#135)

---
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/SubscriberAdditionalInfo.java
@@ -0,0 +1,12 @@
+package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
+
+public class SubscriberAdditionalInfo {
+    public static final String FILE_NUMBER = "365.29";
+    public static final String SEQUENCE = "#.01";
+    public static final String PLACE_OF_SERVICE = "#.02";
+    public static final String DIAGNOSIS = "#.03";
+    public static final String QUALIFIER = "#.04";
+    public static final String NATURE_OF_INJURY_CODE = "#.05";
+    public static final String NATURE_OF_INJURY_CATEGORY = "#.06";
+    public static final String NATURE_OF_INJURY_TEXT = "#.07";
+}

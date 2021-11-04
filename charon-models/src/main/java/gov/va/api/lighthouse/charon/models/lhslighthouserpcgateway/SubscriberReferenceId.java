ece83aa770da68449d790f9af7b96c72d3af0cee
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-11-04 13:23:01 -0400

    Subscriber Reference Id Model (#134)

---
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/SubscriberReferenceId.java
@@ -0,0 +1,9 @@
+package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
+
+public class SubscriberReferenceId {
+  public static final String FILE_NUMBER = "365.291";
+  public static final String SEQUENCE = "#.01";
+  public static final String REFERENCE_ID = "#.02";
+  public static final String REFERENCE_ID_QUALIFIER = "#.03";
+  public static final String DESCRIPTION = "#.04";
+}

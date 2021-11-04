98aff74a587208c7fc0efed34e275888c9706ad4
bryan.schofield <vasdvp.jenkins@libertyits.com>
2021-11-04 19:30:54 +0000

    Release 4.0.53 - Jenkins Build 174 (department-of-veterans-affairs/lighthouse-charon/main)

--- charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/HealthcareServicesDelivery.java
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/HealthcareServicesDelivery.java
@@ -1,14 +1,14 @@
 package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
 
 public class HealthcareServicesDelivery {
-    public static final String FILE_NUMBER = "365.27";
-    public static final String SEQUENCE = "#.01";
-    public static final String BENEFIT_QUANTITY = "#02";
-    public static final String QUANTITY_QUALIFIER = "#.03";
-    public static final String SAMPLE_SELECTION_MODULUS = "#.04";
-    public static final String UNITS_OF_MEASUREMENT = "#.05";
-    public static final String TIME_PERIODS = "#.06";
-    public static final String TIME_PERIOD_QUALIFIER = "#.07";
-    public static final String DELIVERY_FREQUENCY = "#.08";
-    public static final String DELIVERY_PATTERN = "#.09";
+  public static final String FILE_NUMBER = "365.27";
+  public static final String SEQUENCE = "#.01";
+  public static final String BENEFIT_QUANTITY = "#02";
+  public static final String QUANTITY_QUALIFIER = "#.03";
+  public static final String SAMPLE_SELECTION_MODULUS = "#.04";
+  public static final String UNITS_OF_MEASUREMENT = "#.05";
+  public static final String TIME_PERIODS = "#.06";
+  public static final String TIME_PERIOD_QUALIFIER = "#.07";
+  public static final String DELIVERY_FREQUENCY = "#.08";
+  public static final String DELIVERY_PATTERN = "#.09";
 }

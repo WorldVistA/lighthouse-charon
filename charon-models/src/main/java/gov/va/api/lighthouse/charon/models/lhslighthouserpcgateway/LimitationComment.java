d7e4e9c2f7b4142658b7ee15d0d4a08729ecef9d
Evan Clendenning <evan.clendenning@libertyits.com>
2021-11-22 11:08:53 -0500

    limitation comment file (#139)

---
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/lhslighthouserpcgateway/LimitationComment.java
@@ -0,0 +1,6 @@
+package gov.va.api.lighthouse.charon.models.lhslighthouserpcgateway;
+
+public class LimitationComment {
+  public static final String FILE_NUMBER = "355.321";
+  public static final String LIMITATION_COMMENT = "#.01";
+}
